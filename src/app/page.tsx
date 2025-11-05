c76049a66937d11306956609de889105ab323de4
exrsxe <nigazzz2000@gmail.com>
2025-11-05 17:44:44 +0700

    fix: удалил импорт кнопки

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Image from "next/image";
 import Link from "next/Link";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/lib/ui/accordion";
-import { Button } from "@/lib/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-center uppercase">Главная страница</h1>
-      <div className="container">
-        <Button variant="orange">123</Button>
-      </div>
     </div>
   );
 }
