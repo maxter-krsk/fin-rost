ad559ed730e78a3eb70b5579e800cb8d5c244472
exrsxe <nigazzz2000@gmail.com>
2025-11-07 15:36:15 +0700

    fix: Убрал блок со страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { ThankfulLetter } from "./components/sections/Home/thankful-letter/ThankfulLetter";
-
 export default function Home() {
   return (
     <div>
       <h1 className="text-center uppercase">Главная страница</h1>
-      <ThankfulLetter />
     </div>
   );
 }
