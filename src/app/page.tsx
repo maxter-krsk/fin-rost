ecf8fffae89446d689a19cf55b5c153eac4df729
exrsxe <nigazzz2000@gmail.com>
2025-11-10 09:30:38 +0700

    feat: адаптированный блок с сертификатами

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Certificates } from "./components/sections/Home/certificates/Certificates";
+import { ThankfulLetter } from "./components/sections/Home/thankful-letter/ThankfulLetter";
 export default function Home() {
   return (
     <div>
       <h1 className="text-center uppercase">Главная страница</h1>
+      <Certificates />
+      <ThankfulLetter />
     </div>
   );
 }
