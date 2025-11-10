e5fbd96aa3335ede2b28cf9c76173d6a1882bcda
exrsxe <nigazzz2000@gmail.com>
2025-11-10 12:10:58 +0700

    fix: убрал блоки с главной страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Form } from "./components/sections/Home/form/Form";
-import { Certificates } from "./components/sections/Home/certificates/Certificates";
-import { ThankfulLetter } from "./components/sections/Home/thankful-letter/ThankfulLetter";
 export default function Home() {
   return (
     <div>
       <h1 className="text-center uppercase">Главная страница</h1>
-      <Form />
-      <Certificates />
-      <ThankfulLetter />
     </div>
   );
 }
