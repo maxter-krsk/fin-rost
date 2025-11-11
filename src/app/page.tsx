4e216ef10b255de67d677444f67cdb147c4db72b
Kirill Khaylenko <alexfrol219@gmail.com>
2025-11-11 15:03:50 +0700

    Убрал тестирумые элементы со страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import CircleElement from "@/app/components/ui/CircleElement";
-
 export default function Home() {
   return (
     <div className="container">
       <h1 className="text-center uppercase">Главная страница</h1>
-      <div className="mb-100px bg-darkBlue mt-[100px] flex h-[300px] w-[300px] items-center justify-center border-[1px] border-solid border-black">
-        <CircleElement>
-          <span>
-            1
-          </span>
-        </CircleElement>
-      </div>
     </div>
   );
 }
