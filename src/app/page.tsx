eda761dea1b2f4f4f7e640a0528273b6c71f7d7e
exrsxe <nigazzz2000@gmail.com>
2025-11-11 15:16:01 +0700

    fix: Удалил импорт хиро блока со страницы

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import { Hero } from "./components/sections/Home/hero/Hero";
 export default function Home() {
   return (
-    <div>
       <h1 className="text-center uppercase">Главная страница</h1>
-    </div>
   );
 }
