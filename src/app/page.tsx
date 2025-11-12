8df2c43ee58276fc601095a51fadaf602cf7e626
Kirill Khaylenko <alexfrol219@gmail.com>
2025-11-12 17:05:54 +0700

    feat: Добавил реализацию карточек секции Проблем роста || Стилизовал и адаптировал блок || Добавил размеры, начертания, стиль текста для карточек и также проадаптировал

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { BarrierSection } from "./components/sections/Home/barriers/BarrierSection";
+
 export default function Home() {
   return (
+    <div>
       <h1 className="text-center uppercase">Главная страница</h1>
+      <BarrierSection />
+    </div>
   );
 }
