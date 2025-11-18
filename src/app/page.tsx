6f4987feaeed049a9da8c540e7883580558018c0
Kirill Khaylenko <alexfrol219@gmail.com>
2025-11-18 12:53:00 +0700

    chore: Убрал добавление компонента к странице

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Solutions } from "./components/sections/Home/solutions/Solutions";
-
 export default function Home() {
   return (
     <div>
       <h1 className="text-center uppercase">Главная страница</h1>
-      <Solutions />
     </div>
   );
 }
