f01f7087ad97d98f6d9684089852f34a51aae4c3
Kirill Khaylenko <alexfrol219@gmail.com>
2025-11-27 17:31:49 +0700

    fix: Убрал отображение блока Прайса на странице

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { PriceSection } from "./components/sections/Home/price/PriceSection";
-
 export default function Home() {
   return (
     <div>
       <h1 className="text-center uppercase">Главная страница</h1>
-      <PriceSection />
     </div>
   );
 }
